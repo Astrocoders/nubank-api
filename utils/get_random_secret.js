30c807857a43c7f5f8546bb6096876fa81b4470b
Gabriel Rubens <grsabreu@gmail.com>
2016-10-20 23:45:36 -0200

    Remove unnecessary files

--- utils/get_random_secret.js
+++
@@ -1,7 +0,0 @@
-import { times, random } from 'lodash'
-
-const dict = 'qwertyuiopasdfghjklzxcvbnm1234567890_'
-
-export default function getRandomSecret(){
-  return times(32, () => dict[random(dict.length-1, false)]).join('')
-}
